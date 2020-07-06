8872f749fa550d3dc939e87e97696960e14a7c9c
Melissa Lopez Valdez <melissa.melisita@gmail.com>
2020-07-06 15:49:57 +0000

    add static folder assets

--- all.js
+++ all.js
@@ -3,6 +3,7 @@ const path = require('path')
 const app = express()
 
 app.use(express.static("./views"));
+app.use(express.static("./assets"));
 
 app.get("/home", (req, res) => {
  res.status(200).sendFile (path.join (__dirname, "/views/apihome.html"));
