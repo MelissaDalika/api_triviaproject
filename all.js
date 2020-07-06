2958af5d4149154d8dc2f4adfdbd077fb29740f3
Melissa Lopez Valdez <melissa.melisita@gmail.com>
2020-07-06 08:59:42 +0000

    add port and response home html

--- all.js
+++ all.js
@@ -2,3 +2,11 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.get("/home", (req, res) => {
+ res.status(200).sendFile (path.join (__dirname, "/apihome.html"));
+});
+
+
+app.listen( 3000, () => console.log('Listening on port 3000'));
+
+
