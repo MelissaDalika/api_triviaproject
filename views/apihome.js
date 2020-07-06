3f8b67722e0828a05b80c9db1a9f7e1a09e5e821
Piovan Andrea <littlehooah@gmail.com>
2020-07-06 10:53:32 +0000

    implementata funzione shuffle sull'array delle risposte

--- views/apihome.js
+++ views/apihome.js
@@ -8,6 +8,36 @@ axios
   
 
 function questionBuilder(questionArr) {
-  document.getElementById("question").innerHTML = questionArr[0].question;
+  let questionText = questionArr[0].question;
+  let correctAnswer = questionArr[0].correct_answer;
+  let wrongAnswers = questionArr[0].incorrect_answers;
+
+  let possibleAnswers = wrongAnswers.concat(correctAnswer);
+  console.log(possibleAnswers);
+  shuffle(possibleAnswers);
+  console.log(possibleAnswers);
+
+  document.getElementById("question").innerHTML = questionText;
   
+}
+
+
+//Funzione di shuffle presa online che shuffle non esiste di default in js
+function shuffle(arra1) {
+  let ctr = arra1.length;
+  let temp;
+  let index;
+
+  // While there are elements in the array
+  while (ctr > 0) {
+  // Pick a random index
+      index = Math.floor(Math.random() * ctr);
+  // Decrease ctr by 1
+      ctr--;
+  // And swap the last element with it
+      temp = arra1[ctr];
+      arra1[ctr] = arra1[index];
+      arra1[index] = temp;
+  }
+  return arra1;
 }
