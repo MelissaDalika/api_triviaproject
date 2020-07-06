2f07017a88bccb3c62e6f620151e94aa9d009eeb
Piovan Andrea <littlehooah@gmail.com>
2020-07-06 10:37:54 +0000

    Collegamento axios e base funzione di costruzione html

--- views/apihome.js
+++ views/apihome.js
@@ -1,1 +1,13 @@
-alert("hello world!")
+
+axios
+  .get("https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple")
+  .then((response) => {
+    let question = response.data.results;
+    questionBuilder(question)
+  });
+  
+
+function questionBuilder(questionArr) {
+  document.getElementById("question").innerHTML = questionArr[0].question;
+  
+}
