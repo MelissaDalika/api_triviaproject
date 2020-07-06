22d37d6085be2b53fa7518230783eebb12b105e0
Andrea Salomon <salozzo.andrea@gmail.com>
2020-07-06 09:46:30 +0000

    add express static folder views

--- all.js
+++ all.js
@@ -2,10 +2,11 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.get("/home", (req, res) => {
- res.status(200).sendFile (path.join (__dirname, "/apihome.html"));
-});
+app.use(express.static("./views"));
 
+app.get("/home", (req, res) => {
+ res.status(200).sendFile (path.join (__dirname, "/views/apihome.html"));
+});
 
 app.listen( 3000, () => console.log('Listening on port 3000'));
 
