eca8801d6cdcbdaec7adc36a273d846c30461628
Melissa Lopez Valdez <melissa.melisita@gmail.com>
2020-07-07 10:47:29 +0000

    add window location

--- views/apihome.js
+++ views/apihome.js
@@ -1,16 +1,16 @@
-let currentQuestion= 0;
-let infoQuestions = {}
+let currentQuestion = 0;
+let infoQuestions = {};
 let currentPoints = 0;
 
 //Costructor HTML che richiede lista categoria/id all'API e forma le relative opzioni dentro al form con id "category"
 
 function getCategoryOptions() {
-  let categoryArr
+  let categoryArr;
   axios
     .get("https://opentdb.com/api_category.php")
-    .then((response) => categoryArr = response.data.trivia_categories)
-    .then(() =>console.log(categoryArr))
-    .then(() => buildCategoryOptions(categoryArr))
+    .then((response) => (categoryArr = response.data.trivia_categories))
+    .then(() => console.log(categoryArr))
+    .then(() => buildCategoryOptions(categoryArr));
 }
 
 function buildCategoryOptions(dataArr) {
@@ -22,10 +22,8 @@ function buildCategoryOptions(dataArr) {
   }
 }
 
-
 //Chiamata della funzione per le opzioni
-getCategoryOptions()
-
+getCategoryOptions();
 
 //Composizione dell'URL e chiamata all'API con le opzioni come query
 function urlComposer() {
@@ -35,58 +33,56 @@ function urlComposer() {
   let questionsTypeValue = document.getElementById("questionsType").value;
   let composedUrl = `https://opentdb.com/api.php?amount=${questionsNbrValue}&category=${categoryValue}&difficulty=${difficultyValue}&type=${questionsTypeValue}`;
   document.getElementById("pTest").innerText = composedUrl;
-  apiRequest(composedUrl)
+  apiRequest(composedUrl);
 }
-
-
 
 //Chiamata all'API con ricezione delle domande
 function apiRequest(url) {
-axios
-  .get(url)
-  .then((response) => {
+  axios.get(url).then((response) => {
     infoQuestions.questions = response.data.results;
     questionBuilder(infoQuestions.questions);
     createRecapList();
-});
+  });
 }
-
 
 //Creazione della tabella di riepilogo in funzione del n° di domande
 function createRecapList() {
   currentQuestion = 0;
   document.getElementById("recapList").innerHTML = ""; //svuota il Div ogni iterazione
   let recapList = document.createElement("ul");
-  recapList.setAttribute("class", "list-group list-group-horizontal justify-content-center p-4");
-  
+  recapList.setAttribute(
+    "class",
+    "list-group list-group-horizontal justify-content-center p-4"
+  );
+
   for (let i = 0; i < infoQuestions.questions.length; i++) {
-    recapList.innerHTML += `<li class="list-group-item">${i+1}</li>`
+    recapList.innerHTML += `<li class="list-group-item">${i + 1}</li>`;
   }
 
   document.getElementById("recapList").appendChild(recapList);
 }
 
-
-//Creazione dei Radio contententi le risposte 
+//Creazione dei Radio contententi le risposte
 function questionBuilder(questionArr) {
   infoQuestions.questionText = questionArr[currentQuestion].question;
   infoQuestions.correctAnswer = questionArr[currentQuestion].correct_answer;
   infoQuestions.wrongAnswers = questionArr[currentQuestion].incorrect_answers;
-  console.log("questionBuilder sta andando")
-  let possibleAnswers = infoQuestions.wrongAnswers.concat(infoQuestions.correctAnswer);
+  console.log("questionBuilder sta andando");
+  let possibleAnswers = infoQuestions.wrongAnswers.concat(
+    infoQuestions.correctAnswer
+  );
   shuffle(possibleAnswers);
- 
 
   document.getElementById("question").innerHTML = infoQuestions.questionText;
 
   let container = document.getElementById("radiaContainer");
   container.innerHTML = "";
-  for(let i=0; i<possibleAnswers.length; i++){
+  for (let i = 0; i < possibleAnswers.length; i++) {
     container.innerHTML += `
     <div class="form-check">
     <input type="radio" id="answer${i}" class="radioInput" name="triviaAnswers"   value="${possibleAnswers[i]}">
     <label for="answer${i}" id="answer${i}label">${possibleAnswers[i]}</label>
-    </div>`
+    </div>`;
   }
 }
 
@@ -95,63 +91,66 @@ function getRadioVal() {
   let selectedRadioValue;
   // get list of radio buttons with specified name
   let radiosArr = document.getElementsByClassName("radioInput");
-  
+
   // loop through list of radio buttons
   for (let i = 0; i < radiosArr.length; i++) {
-      if ( radiosArr[i].checked ) { // radio checked?
-        console.log("getRadioVal sta andando");
-        selectedRadioValue = document.getElementById(`answer${i}label`).innerText; // if so, hold its value in val
-          break; // and break out of for loop
-      }
+    if (radiosArr[i].checked) {
+      // radio checked?
+      console.log("getRadioVal sta andando");
+      selectedRadioValue = document.getElementById(`answer${i}label`).innerText; // if so, hold its value in val
+      break; // and break out of for loop
+    }
   }
   return selectedRadioValue; // return value of checked radio or undefined if none checked
 }
 
-
 //Se risposta giusta aumenta punteggio di 1, altrimenti -1. Cambia colore tabella riepilogo. Aumenta contatore domanda e carica nuova domanda. Se domande esaurite finisce gioco.
 function checkIfRight(inputWord) {
-  let selectedRecapListCell = document.getElementById("recapList").getElementsByTagName("ul")[0].getElementsByTagName("li")[currentQuestion];
+  let selectedRecapListCell = document
+    .getElementById("recapList")
+    .getElementsByTagName("ul")[0]
+    .getElementsByTagName("li")[currentQuestion];
 
   if (inputWord == infoQuestions.correctAnswer) {
-    currentPoints += 2
-    selectedRecapListCell.setAttribute("style","background-color: green;");
-  } 
-  else if (inputWord == "skip"){
-    selectedRecapListCell.setAttribute("style","background-color: yellow;");
-  }
-  else { 
-    currentPoints--; 
-    selectedRecapListCell.setAttribute("style","background-color: red;");
-  }
-  
-
-  if (currentQuestion == (infoQuestions.questions.length-1)) {
-    alert(`domande finite con punteggio ${currentPoints}. Necessario redirect a pagina esterna o costruzione di elemento html nuovo`)
+    currentPoints += 2;
+    selectedRecapListCell.setAttribute("style", "background-color: green;");
+  } else if (inputWord == "skip") {
+    selectedRecapListCell.setAttribute("style", "background-color: yellow;");
   } else {
-  currentQuestion++ ;
-  questionBuilder(infoQuestions.questions);
-  console.log(currentPoints);
+    currentPoints--;
+    selectedRecapListCell.setAttribute("style", "background-color: red;");
+  }
+
+  if (currentQuestion == infoQuestions.questions.length - 1) {
+    alert(
+      `domande finite con punteggio ${currentPoints}. Necessario redirect a pagina esterna o costruzione di elemento html nuovo`
+    );
+      window.location.href = `/results.html?result=${currentPoints}`
+       
+  } else {
+    currentQuestion++;
+    questionBuilder(infoQuestions.questions);
+    console.log(currentPoints);
   }
 }
 
 // NEXT CHANCE FOR SKIP QUESTION
-  let chanceForSkipQuestion = () => {
-
- }
+let chanceForSkipQuestion = () => {};
 
 //Event Listener BOTTONI
 //Bottone conferma risposta
-document.getElementById("confirm").addEventListener("click", () =>{
+document.getElementById("confirm").addEventListener("click", () => {
   checkIfRight(getRadioVal());
 });
 
-document.getElementById("urlComposer").addEventListener("click", () => urlComposer());
+document
+  .getElementById("urlComposer")
+  .addEventListener("click", () => urlComposer());
 
 //Bottone salta domanda
-document.getElementById("skip").addEventListener("click", () =>{
+document.getElementById("skip").addEventListener("click", () => {
   checkIfRight("skip");
 });
-
 
 //Funzione di shuffle presa online che shuffle non esiste di default in js
 function shuffle(arra1) {
@@ -161,18 +160,17 @@ function shuffle(arra1) {
 
   // While there are elements in the array
   while (ctr > 0) {
-  // Pick a random index
-      index = Math.floor(Math.random() * ctr);
-  // Decrease ctr by 1
-      ctr--;
-  // And swap the last element with it
-      temp = arra1[ctr];
-      arra1[ctr] = arra1[index];
-      arra1[index] = temp;
+    // Pick a random index
+    index = Math.floor(Math.random() * ctr);
+    // Decrease ctr by 1
+    ctr--;
+    // And swap the last element with it
+    temp = arra1[ctr];
+    arra1[ctr] = arra1[index];
+    arra1[index] = temp;
   }
   return arra1;
 }
-
 
 /* function checkNextQuestion (){
   for(let i=0; i<4; i++){
