8ac5f69d7d77f866c7d4453e2d7c2c5cda2457de
Piovan Andrea <littlehooah@gmail.com>
2020-07-09 12:09:25 +0000

    leftTable linked to Ranked results only

--- views/results.js
+++ views/results.js
@@ -4,7 +4,8 @@ function playAudio(url) {
 }
 
 
-//RICHIESTA DI TIPO GET AL DATABASE
+//RICHIESTA DI TIPO GET AL DATABASE per costruzione card centrale
+function getPlayerData() {
   var data = null;
   
   var xhr = new XMLHttpRequest();
@@ -14,7 +15,6 @@ function playAudio(url) {
     if (this.readyState === 4) {
       let arrData = JSON.parse(this.responseText);
       console.log(arrData);
-      tableSxBuilder(arrData);
       cardCenterBuilder(arrData);
     }
   });
@@ -25,23 +25,45 @@ function playAudio(url) {
   xhr.setRequestHeader("cache-control", "no-cache");
   
   xhr.send(data);
+}
+//RICHIESTA DI TIPO GET AL DATABASE per costruzione classifica laterale
+function getRankedScores() {
+var data = null;
+  
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    let arrData = JSON.parse(this.responseText);
+    console.log(arrData);
+    tableSxBuilder(arrData);
+  }
+});
+
+xhr.open("GET", "https://trivialapp-043f.restdb.io/rest/punteggio-giocatori?q={%22ranked%22:true}&h={%22$orderby%22:%20{%22results%22:%20-1}}");
+xhr.setRequestHeader("content-type", "application/json");
+xhr.setRequestHeader("x-apikey", "5f046bd8a529a1752c476e5d");
+xhr.setRequestHeader("cache-control", "no-cache");
+
+xhr.send(data);
+}
+
 
 //FUNZIONI CHE USANO LA RICHIESTA GET AL DATABASE  
-  let arrayProva = [
-  {"_id":"5f04791a498ad7680006e2f2","playerName":"Ciccio","results":10},
-  {"_id":"5f047935498ad7680006e2f8","playerName":"Banano","results":50},
-  {"_id":"5f048286498ad7680006eba1","playerName":"Giangiulioberto","results":99},
-  {"_id":"5f047fb2498ad7680006e947","playerName":"Giangiulioberto","results":99}];
-    
-    function cardCenterBuilder(arr) {
-      arr_id = arr.map(element => element.ID);
-      let num = arr_id.indexOf(Math.max(...arr_id));
-      document.getElementsByClassName("card-title")[0].innerText = arr[num].playerName;
-      document.getElementsByClassName("card-text")[0].innerText = `Il tuo punteggio è stato ${arr[num].results}`;
-    }
+      function cardCenterBuilder(arr) {
+        arr_id = arr.map(element => element.ID);
+        let num = arr_id.indexOf(Math.max(...arr_id));
+        document.getElementsByClassName("card-title")[0].innerText = arr[num].playerName;
+        document.getElementsByClassName("card-text")[0].innerText = `Il tuo punteggio è stato ${arr[num].results}`;
+      }
 
     function tableSxBuilder(arr) {
-        for (let i=0; i < arr.length; i++) {
+      let stopNumber = arr.length -1
+        for (let i=0; i < 10; i++) {
+          if ( stopNumber == i) {
+            break
+          }
           document.getElementById("tableSxBody").innerHTML += 
                   `<tr>
                     <th scope="row">${i+1}</th>
@@ -50,3 +72,6 @@ function playAudio(url) {
                   </tr>`
         }
     }
+
+  getPlayerData();
+  getRankedScores();
